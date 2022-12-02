97bea3ed7bdfbbb050eb20b260e7dc871880eb03
Eric lifka <eric.lifka@glassdoor.com>
2022-12-02 10:00:47 -0500

    day 1

---
+++ 01.mjs
@@ -0,0 +1,12 @@
+import { input } from './input/01.mjs'
+
+let result = input
+  .trim()
+  .split('\n\n')
+  .map( elf => elf.split('\n')
+                  .map( num => parseInt(num, 10) )
+                  .reduce( (a, b) => a + b, 0 ) )
+  .sort( (a, b) => b - a )
+
+console.log(result[0])
+console.log(result[0] + result[1] + result[2])
