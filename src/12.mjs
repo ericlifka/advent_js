9f146664369d3e737ac20edcfb0a5ff7a4978fb2
Eric lifka <eric.lifka@glassdoor.com>
2022-12-12 15:30:58 -0500

    day 12 part 2

--- src/12.mjs
+++ src/12.mjs
@@ -1,51 +1,42 @@
 import { input } from '../input/12.mjs'
 
-const neighborsOf = ({x, y}) => [ {x, y: y + 1}, {x, y: y - 1}
-                                , {y, x: x + 1}, {y, x: x - 1} ]
+let inputHeights = input.trim().split('\n').map( line => line.split('') )
+let START = inputHeights.reduce((location, row, y) => location || row.reduce((location, ch, x) => ch == 'S' ? ({x, y}) : location, null), null)
+let END = inputHeights.reduce((location, row, y) => location || row.reduce((location, ch, x) => ch == 'E' ? ({x, y}) : location, null), null)
+let allStarts = inputHeights.reduce((starts, row, y) => row.reduce((starts, ch, x) => ch == 'S' || ch == 'a' ? [...starts, {x, y}] : starts, starts), [])
 
-let start, end
-let heights = input
-  .trim()
-  .split('\n')
-  .map((line, y) => line
-    .split('')
-    .map((ch, x) => {
-      if (ch == 'S') {
-        start = { x, y }
-        return 1
-      } else if (ch == 'E') {
-        end = { x, y }
-        return 26
-      } else {
-        return ch.charCodeAt(0) - 'a'.charCodeAt(0) + 1
+const getHeights = () => inputHeights.map( row => row.map( ch => ch == 'S' ? 1 : ch == 'E' ? 26 : ch.charCodeAt(0) - 'a'.charCodeAt(0) + 1 ))
+const getSteps = start => inputHeights.map((line, y) => line.map((_, x) => y == start.y && x == start.x ? 0 : Number.MAX_SAFE_INTEGER ))
+const getNeighbors = ({x, y}) => [ {x, y: y + 1}, {x, y: y - 1} , {y, x: x + 1}, {y, x: x - 1} ]
+
+const stepsFromStartToEnd = (start, end) => {
+  let heights = getHeights()
+  let steps = getSteps(start)
+  let queue = [ {...start} ]
+
+  while (queue.length > 0) {
+    let position = queue.shift()
+  
+    let height = heights[position.y][position.x]
+    let stepsTo = steps[position.y][position.x]
+    let neighbors = getNeighbors(position)
+  
+    neighbors.forEach( n => {
+      if (  heights[n.y] && heights[n.y][n.x] // it exists
+         && heights[n.y][n.x] <= height + 1   // it's at most one step heigher
+         && steps[n.y][n.x] > stepsTo + 1 )   // it hasn't already been visited with a shorter path
+      {
+        steps[n.y][n.x] = stepsTo + 1
+        queue.push( {...n} )
       }
-    }))
+    })
+  }
 
-let steps = heights.map( line => line.map( _ => Number.MAX_SAFE_INTEGER ))
-steps[start.y][start.x] = 0
-
-let queue = [ {...start} ]
-
-while (queue.length > 0) {
-  let position = queue.shift()
-
-  let height = heights[position.y][position.x]
-  let stepsTo = steps[position.y][position.x]
-  let neighbors = neighborsOf(position)
-
-  neighbors.forEach( n => {
-    if (  heights[n.y] && heights[n.y][n.x] // it exists
-       && heights[n.y][n.x] <= height + 1   // it's at most one step heigher
-       && steps[n.y][n.x] > stepsTo + 1 )   // it hasn't already been visited with a shorter path
-    {
-      steps[n.y][n.x] = stepsTo + 1
-      queue.push( {...n} )
-    }
-  })
+  return steps[end.y][end.x]
 }
 
-// // console.log(heights, steps)
-// heights.forEach( row => console.log(row.join(`,\t`)))
-// console.log('\n')
-// steps.forEach( row => console.log(row.join(`,\t`)))
-console.log(steps[end.y][end.x])
+
+let part1 = stepsFromStartToEnd(START, END)
+let part2 = allStarts.map( start => stepsFromStartToEnd(start, END) ).sort()[0]
+
+console.log(part1, part2)
