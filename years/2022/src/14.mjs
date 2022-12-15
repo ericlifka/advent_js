060c204d38b0ea6fbc27518d8c2d15ac9eccbdfb
Eric lifka <eric.lifka@glassdoor.com>
2022-12-15 14:28:41 -0500

    day 14 cleanup

--- years/2022/src/14.mjs
+++ years/2022/src/14.mjs
@@ -8,11 +8,6 @@ const createKey = (x, y) => {
            , ymin: Math.min(bounds.ymin, y), ymax: Math.max(bounds.ymax, y) }
   return getKey(x, y)
 }
-const sandKey = (x, y) => {
-  bounds.xmin = Math.min(bounds.xmin, x)
-  bounds.xmax = Math.max(bounds.xmax, x)
-  return getKey(x, y)
-}
 
 input.trim()
   .split('\n')
@@ -23,8 +18,7 @@ input.trim()
     cave[createKey(x, y)] = '#'
     while (path.length > 0) {
       let [tx, ty] = path.shift()
-      let dx = cap(tx - x)
-      let dy = cap(ty - y)
+      let dx = cap(tx - x),  dy = cap(ty - y)
       while (x != tx || y != ty) {
         x += dx, y += dy
         cave[createKey(x, y)] = '#'
@@ -36,48 +30,14 @@ let count = 0
 while (true) {
   let sx = 500, sy = 0
   while (true) {
-    if (sy == bounds.ymax + 1) {
-      cave[getKey(sx, sy)] = 'o'
-      count++
-      break
-    }
-
-    if (!cave[getKey(sx, sy + 1)]) {
-      sy += 1
-    }
-    else if (!cave[getKey(sx - 1, sy + 1)]) {
-      sy += 1, sx -= 1
-    }
-    else if (!cave[getKey(sx + 1, sy + 1)]) {
-      sy += 1, sx += 1
-    } 
-    else {
-      cave[sandKey(sx, sy)] = 'o'
-      count++
-      break
-    }
+    if      (sy == bounds.ymax + 1)         break
+    else if (!cave[getKey(sx, sy + 1)])     sy += 1
+    else if (!cave[getKey(sx - 1, sy + 1)]) sy += 1, sx -= 1
+    else if (!cave[getKey(sx + 1, sy + 1)]) sy += 1, sx += 1
+    else                                    break
   }
-
-  if (sy == 0) {
-    break
-  }
+  cave[getKey(sx, sy)] = 'o', count++
+  if (sy == 0) break
 }
 
-printCave()
 console.log(count)
-
-
-function printCave() {
-  let printStr = ''
-  for (let y = bounds.ymin; y <= bounds.ymax + 1; y++) {
-    for (let x = bounds.xmin - 2; x <= bounds.xmax + 2; x++) {
-      printStr += cave[getKey(x, y)] || '.'
-    }
-    printStr += '\n'
-  }
-  for (let x = bounds.xmin - 2; x <= bounds.xmax + 2; x++) {
-    printStr += '#'
-  }
-  printStr += '\n\n'
-  console.log(printStr)
-}
