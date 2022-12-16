7613858c6367533821521466c03ead3dae9c2888
Eric lifka <eric.lifka@glassdoor.com>
2022-12-15 22:25:48 -0500

    part 2 take two, a little bit faster and a little bit worse

--- years/2022/src/15.mjs
+++ years/2022/src/15.mjs
@@ -31,25 +31,85 @@ let sensors = input.trim().split('\n')
   .map(([ _, sx, sy, bx, by ]) => [ sx, sy, bx, by ].map( d => parseInt(d, 10) ))
   .map(([ sx, sy, bx, by ]) => ({ sx, sy, manhattan: manhattanDistance(sx, sy, bx, by) }))
 
-const withinRange = (x, y) => {
-  for (let { sx, sy, manhattan } of sensors) {
-    if (manhattanDistance(sx, sy, x, y) <= manhattan)
-      return true
-  }
-  return false
-}
-
-const findOutOfRange = () => {
-  for (let x = 0; x <= targetSize; x++) {
-    // if (x % 1000 == 0) {
-      console.log("x:", x)
-    // }
-    for (let y = 0; y <= targetSize; y++) {
-      if (!withinRange(x, y)) {
-        return x * 4000000 + y
-      }
+let row = new Array(targetSize)
+const checkRow = y => {
+  row.fill(true)
+  sensors.forEach(({ sx, sy, manhattan }) => {
+    let sensorYdistance = Math.abs(sy - y)
+    let xTravel = manhattan - sensorYdistance
+    if (xTravel >= 0) {
+      row.fill(false, Math.max(sx - xTravel, 0), Math.min(sx + xTravel, targetSize))
+      // for (let x = Math.max(sx - xTravel, 0); x <= Math.min(sx + xTravel, targetSize); x++) {
+      //   row[x] = false
+      // }
     }
+  })
+  // console.log(row)
+  let stillOpen = row.findIndex( v => v )
+  if (stillOpen != -1) {
+    return { x: stillOpen, y: y }
+  } else {
+    return null
   }
 }
 
-console.log('part2', findOutOfRange())
+let start = Date.now()
+let lastTime = Date.now()
+for (let y = 0; y < targetSize; y++) {
+  if (y % 100 == 0) {
+    let n = Date.now()
+    console.log(y, n - lastTime, 'ms', (n - start) / 1000, 's')
+    lastTime = n
+  }
+  
+  let result = checkRow(y)
+  if (result) {
+    console.log(result, result.x * 4000000 + result.y)
+    break
+  }
+}
+
+// let s = Date.now()
+// console.log('starting grid creation')
+// let grid = new Array(targetSize).fill().map( _ => new Array(targetSize).fill(true) )
+// console.log('finished grid creation', (Date.now() - s) / 1000)
+
+// input.trim().split('\n')
+//   .map( line => line.match(/Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/) )
+//   .map(([ _, sx, sy, bx, by ]) => [ sx, sy, bx, by ].map( d => parseInt(d, 10) ))
+//   .map(([ sx, sy, bx, by ]) => ({ sx, sy, manhattan: manhattanDistance(sx, sy, bx, by) }))
+//   // .forEach(({ sx, sy, manhattan }) => {
+//   //   if (sx - manhattan <= 0) {
+//   //     console.log(`0 | (x=${sx}, y=${sy}) -> ${manhattan}`)
+//   //   }
+//   //   if (  sx + manhattan >= targetSize) {
+//   //     console.log(`${targetSize} | (x=${sx}, y=${sy}) -> ${manhattan}`)
+//   //   }
+//   // })
+//   .sort((l, r) => l.manhattan - r.manhattan)
+//   .forEach(({ sx, sy, manhattan }) => {
+//     console.log(`(${sx}, ${sy}) -> ${manhattan}`)
+//   })
+
+// const withinRange = (x, y) => {
+//   for (let { sx, sy, manhattan } of sensors) {
+//     if (manhattanDistance(sx, sy, x, y) <= manhattan)
+//       return true
+//   }
+//   return false
+// }
+
+// const findOutOfRange = () => {
+//   for (let x = 0; x <= targetSize; x++) {
+//     // if (x % 1000 == 0) {
+//       console.log("x:", x)
+//     // }
+//     for (let y = 0; y <= targetSize; y++) {
+//       if (!withinRange(x, y)) {
+//         return x * 4000000 + y
+//       }
+//     }
+//   }
+// }
+
+// console.log('part2', findOutOfRange())
