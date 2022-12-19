8ded2609defcbcd76489ea4e77f5b211990c4f4c
Eric lifka <eric.lifka@glassdoor.com>
2022-12-18 20:59:06 -0500

    day 18 part 2

--- years/2022/src/18.mjs
+++ years/2022/src/18.mjs
@@ -1,22 +1,36 @@
 import { input } from '../input/18.mjs'
 
+let min = -1, max = 20
 const parseCoord = coord => coord.split(',').map( n => parseInt(n, 10) )
 const encodeCoord = coord => coord.join(',')
 const getNeighbors = ([x, y, z]) =>
- [[x + 1, y, z], [x - 1, y, z],
-  [x, y + 1, z], [x, y - 1, z],
-  [x, y, z + 1], [x, y, z - 1]]
+ [ [x + 1, y, z], [x - 1, y, z]
+ , [x, y + 1, z], [x, y - 1, z]
+ , [x, y, z + 1], [x, y, z - 1]
+ ].filter( coord => coord.every( n => n >= min && n <= max))
+
 
 let shape = new Set(input.trim().split('\n'))
 
+let queue = [ encodeCoord([max, max, max]) ]
+let water = new Set()
+while (queue.length > 0) {
+  let fillAt = queue.shift()
+  if (!water.has(fillAt)) {
+    water.add(fillAt)
+    getNeighbors(parseCoord(fillAt)).forEach( n => {
+      if (!shape.has(encodeCoord(n)))
+        queue.push(encodeCoord(n))
+    })
+  }
+}
+
 let surfaceArea = 0
-for (let coord of shape.values()) {
+for (let coord of water.values()) {
   getNeighbors(parseCoord(coord)).forEach( n => {
-    if (!shape.has(encodeCoord(n))) {
+    if (shape.has(encodeCoord(n)))
       surfaceArea++
-    }
   })
 }
 
-console.log(shape.size)
-console.log('part1:', surfaceArea)
+console.log('surface area:', surfaceArea)
