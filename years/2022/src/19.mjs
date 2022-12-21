11b4cdb6b35b6b21ef43aa65746216673bb37cc7
Eric lifka <eric.lifka@glassdoor.com>
2022-12-21 01:54:51 -0500

    redid day 19 so algorithm fast enough for part2

--- years/2022/src/19.mjs
+++ years/2022/src/19.mjs
@@ -6,126 +6,125 @@ const blueprints = input
   .map(([_, ...values]) => values.map( v => parseInt(v, 10)))
   .map(([ id, oreOre, claOre, obsOre, obsCla, geoOre, geoObs ]) =>  ({ id, oreOre, claOre, obsOre, obsCla, geoOre, geoObs }))
 
-const runSim = (blueprint, target) => {
-  const resetIgnoring = () => ({ ore: false, cla: false, obs: false, geo: false })
-  const copyState = (state = {
-    minutes: 0,
-    resources: { ore: 0, cla: 0, obs: 0, geo: 0 },
-    bots: { ore: 1, cla: 0, obs: 0, geo: 0 },
-    ignoring: resetIgnoring()
-  }) => ({ minutes: state.minutes, resources: {...state.resources}, bots: {...state.bots}, ignoring: {...state.ignoring} })
-  
+const baseState = () => ({
+  minutes: 0,
+  resources: { ore: 0, cla: 0, obs: 0, geo: 0 },
+  bots: { ore: 1, cla: 0, obs: 0, geo: 0 }
+})
+const cloneState = (state = baseState()) => ({
+  minutes: state.minutes,
+  resources: {...state.resources},
+  bots: {...state.bots}
+})
+const stepState = state => {
+  state.minutes += 1
+  state.resources.ore += state.bots.ore
+  state.resources.cla += state.bots.cla
+  state.resources.obs += state.bots.obs
+  state.resources.geo += state.bots.geo
+}
+
+const testBlueprint = (blueprint, target) => {
   let maxOreBots = Math.max(blueprint.oreOre, blueprint.claOre, blueprint.obsOre, blueprint.geoOre)
   let maxClaBots = blueprint.obsCla
-  let maxGeoBots = blueprint.geoObs
-
-  let queue = [ copyState() ]
-  let maxGeodes = 0
-  let maxSequence = null
-  let thresholds = [ 0 ]
+  let maxObsBots = blueprint.geoObs
+  let queue = [ baseState() ]
+  let thresholds = new Array(target).fill(0)
+  let maxGeo = { resources: { geo: 0 } }
   while (queue.length > 0) {
     let state = queue.shift()
-    let production = { ...state.bots }
-    let newStates = [ ]
+    let newStates = []
 
-    if (state.resources.ore >= blueprint.geoOre && state.resources.obs >= blueprint.geoObs) {
-      let buy = copyState(state)
-      buy.resources.ore -= blueprint.geoOre
-      buy.resources.obs -= blueprint.geoObs
-      buy.bots.geo += 1
-      buy.ignoring = resetIgnoring()
-      buy.action = "Building Geode Cracker"
-      newStates.push(buy)
+    if (state.bots.obs >= 1) {
+      let buildGeoState = cloneState(state)
+      while (buildGeoState.resources.ore < blueprint.geoOre || buildGeoState.resources.obs < blueprint.geoObs)
+        stepState(buildGeoState)
+      
+      if (buildGeoState.minutes < target) {
+        stepState(buildGeoState)
+        buildGeoState.resources.ore -= blueprint.geoOre
+        buildGeoState.resources.obs -= blueprint.geoObs
+        buildGeoState.bots.geo += 1
+        buildGeoState.parent = state
+        newStates.push(buildGeoState)
+      }
     }
-    else {
-      if (!state.ignoring.obs && state.bots.geo < maxGeoBots && state.resources.ore >= blueprint.obsOre && state.resources.cla >= blueprint.obsCla) {
-        let buy = copyState(state)
-        buy.resources.ore -= blueprint.obsOre
-        buy.resources.cla -= blueprint.obsCla
-        buy.bots.obs += 1
-        buy.ignoring = resetIgnoring()
-        buy.action = "Building Obsidian miner"
-        newStates.push(buy)
-  
-        let ignore = copyState(state)
-        ignore.ignoring.obs = true
-        newStates.push(ignore)
-      }
 
-      if (!state.ignoring.ore && state.bots.ore < maxOreBots && state.resources.ore >= blueprint.oreOre) {
-        let buy = copyState(state)
-        buy.resources.ore -= blueprint.oreOre
-        buy.bots.ore += 1
-        buy.ignoring = resetIgnoring()
-        buy.action = "Building Ore gatherer"
-        newStates.push(buy)
-  
-        let ignore = copyState(state)
-        ignore.ignoring.ore = true
-        newStates.push(ignore)
+    if (state.bots.obs < maxObsBots && state.bots.cla >= 1) {
+      let buildObsState = cloneState(state)
+      while (buildObsState.resources.ore < blueprint.obsOre || buildObsState.resources.cla < blueprint.obsCla)
+        stepState(buildObsState)
+
+      if (buildObsState.minutes < target) {
+        stepState(buildObsState)
+        buildObsState.resources.ore -= blueprint.obsOre
+        buildObsState.resources.cla -= blueprint.obsCla
+        buildObsState.bots.obs += 1
+        buildObsState.parent = state
+        newStates.push(buildObsState)
       }
-  
-      if (!state.ignoring.cla && state.bots.cla < maxClaBots && state.resources.ore >= blueprint.claOre) {
-        let buy = copyState(state)
-        buy.resources.ore -= blueprint.claOre
-        buy.bots.cla += 1
-        buy.ignoring = resetIgnoring()
-        buy.action = "Building Clay gatherer"
-        newStates.push(buy)
-  
-        let ignore = copyState(state)
-        ignore.ignoring.cla = true
-        newStates.push(ignore)
+    }
+
+    if (state.bots.cla < maxClaBots) {
+      let buildClaState = cloneState(state)
+      while (buildClaState.resources.ore < blueprint.claOre)
+        stepState(buildClaState)
+
+      if (buildClaState.minutes < target) {
+        stepState(buildClaState)
+        buildClaState.resources.ore -= blueprint.claOre
+        buildClaState.bots.cla += 1
+        buildClaState.parent = state
+        newStates.push(buildClaState)
+      }
+    }
+
+    if (state.bots.ore < maxOreBots) {
+      let buildOreState = cloneState(state)
+      while (buildOreState.resources.ore < blueprint.oreOre)
+        stepState(buildOreState)
+
+      if (buildOreState.minutes < target) {
+        stepState(buildOreState)
+        buildOreState.resources.ore -= blueprint.oreOre
+        buildOreState.bots.ore += 1
+        buildOreState.parent = state
+        newStates.push(buildOreState)
       }
     }
 
     if (newStates.length == 0) {
-      newStates.push(copyState(state))
+      let gatherState = cloneState(state)
+      while (gatherState.minutes < target)
+        stepState(gatherState)
+
+      gatherState.parent = state
+      newStates.push(gatherState)
     }
 
-    newStates.forEach( s => {
-      s.minutes += 1
-      s.resources.ore += production.ore
-      s.resources.cla += production.cla
-      s.resources.obs += production.obs
-      s.resources.geo += production.geo
-      s.parent = state
+    newStates.forEach( newState => {
+      if (newState.minutes >= target) {
+        if (newState.resources.geo > maxGeo.resources.geo)
+          maxGeo = newState
+      } else {
+        if (newState.bots.geo >= (thresholds[newState.minutes - 1] || 0)) {
+          for (let i = newState.minutes; i < thresholds.length; i++) {
+            if (thresholds[i] < newState.bots.geo)
+              thresholds[i] = newState.bots.geo
+          }
 
-      if (s.minutes >= target) {
-        if (s.resources.geo > maxGeodes) {
-          maxGeodes = s.resources.geo
-          maxSequence = s
-          // console.log('new max geodes: ', maxGeodes, ' geode crackers: ', s.bots.geo, ' at minute: ', s.minutes)
-        }
-      }
-      else {
-        if (!thresholds[s.minutes] || thresholds[s.minutes] < s.bots.geo) {
-          thresholds[s.minutes] = s.bots.geo
-          // console.log('new threshold ', s.minutes, ':', s.resources.geo)
-        }
-        if (s.bots.geo >= (thresholds[s.minutes - 1] || 0)) {
-          queue.push(s)
+          queue.push(newState)
         }
       }
     })
   }
 
-  let steps = []
-  while (maxSequence) {
-    let nextMax = maxSequence.parent
-    delete maxSequence.parent
-    steps.unshift(maxSequence)
-    maxSequence = nextMax
-  }
-  // console.log(steps)
-  // console.log('blueprint:', blueprint)
-  console.log('max geodes:', maxGeodes)
-  return maxGeodes
+  return maxGeo.resources.geo
 }
 
 let start = Date.now()
-let part1 = blueprints.slice(0, 3).reduce((total, blueprint, i, 
-                              _, __ = console.log(`running blueprint ${blueprint.id}`)) => 
-  runSim(blueprint, 32) * total, 1)
-console.log(part1)
+console.log('part1', blueprints.reduce((total, blueprint) => total + testBlueprint(blueprint, 24) * blueprint.id, 0))
+let checkpoint = Date.now()
+console.log((checkpoint - start)/1000, 's')
+console.log('part2', blueprints.slice(0, 3).reduce((total, blueprint) => total * testBlueprint(blueprint, 32), 1))
 console.log((Date.now() - start)/1000, 's')
